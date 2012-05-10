519181df03d3b2196a8b47fca1dc1a7ba22c30bd
Shawn Rusaw <shawn.rusaw@webfilings.com>
2012-05-10 11:36:22 -0600

    setting namespace based on oath credentials

--- gae_skeleton/appengine_config.py
+++ gae_skeleton/appengine_config.py
@@ -1,8 +1,23 @@
 from google.appengine.api import users
+from google.appengine.api import oauth
 
 # Called only if the current namespace is not set.
 # This sets up the application so that each users data is
 # entirely contained within a namespace.
 def namespace_manager_default_namespace_for_request():
     # assumes the user is logged in.
-    return users.get_current_user().user_id()
+
+    # check the cookies
+    user = users.get_current_user()
+    if user:
+        return user.user_id()
+
+    # check the oauth params
+    try:
+        user = oauth.get_current_user()
+        if user:
+            return user.user_id()
+    except oauth.Error:
+        pass # plain old unauthenticated access, no oauth
+
+    return ""
